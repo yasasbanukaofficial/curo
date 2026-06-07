532b6fd343eeb79d291ddc37aa6b09a7a3b46444
yasas banuka <yixbinfo@gmail.com>
2026-06-07 23:15:48 +0530

    feat: implement Express server with CORS and MongoDB connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.MONGODB_URL as string;
+
+app.get("/", (req, res) => {
+  res.send("CURO API. Unauthorized requests are not allowed.");
+});
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("[DB]: Connected to MongoDB");
+  })
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(
+    `[LOG]: express server started on port ${PORT} - http://localhost:${PORT}`,
+  );
+});
