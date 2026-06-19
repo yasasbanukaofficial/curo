d25d37faeb1bd0197b9b6ea9a2a714bd459fe040
yasas banuka <yixbinfo@gmail.com>
2026-06-19 11:46:19 +0530

    feat(web): add audit logs page
    - Add AuditLogsPage with table showing action, resource, user name, metadata, and date
    
    - Color-coded badges for action (CREATED/UPDATED/DELETED) and resource (SECRET/PROJECT/ENVIRONMENT)
    
    - Show current user name fetched from /auth/me
    
    - Register /audits route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import FormPage from "./pages/FormPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import EnvironmentsPage from "./pages/EnvironmentsPage";
+import AuditLogsPage from "./pages/AuditLogsPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/form" element={<FormPage />} />
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="/environments" element={<EnvironmentsPage />} />
+      <Route path="/audits" element={<AuditLogsPage />} />
     </Routes>
   );
 }
