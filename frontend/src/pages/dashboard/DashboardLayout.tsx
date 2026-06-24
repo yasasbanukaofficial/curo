f418097197675024c86c0a60cd6d0e79b3bcb5d3
yasas banuka <yixbinfo@gmail.com>
2026-06-25 01:30:45 +0530

    fix(dashboard): remove showSettings prop from Sidebar usage in DashboardLayout

--- frontend/src/pages/dashboard/DashboardLayout.tsx
+++ frontend/src/pages/dashboard/DashboardLayout.tsx
@@ -15,7 +15,7 @@ function DashboardInner() {
       <TopNav />
 
       <div className="flex flex-1 min-h-0">
-        <Sidebar showSettings={showSettings} onToggleSettings={setShowSettings} />
+        <Sidebar onToggleSettings={setShowSettings} />
         <main className="flex-1 flex flex-col min-w-0 overflow-y-auto pb-16 lg:pb-0">
           <Outlet />
         </main>
