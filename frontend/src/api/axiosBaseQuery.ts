bd560eb8b207909676ac8f97da39e4db17dc8c72
yasas banuka <yixbinfo@gmail.com>
2026-06-27 00:30:05 +0530

    refactor(frontend): move axiosBaseQuery from lib/ to api/

--- frontend/src/api/axiosBaseQuery.ts
+++ frontend/src/api/axiosBaseQuery.ts
@@ -1,17 +1,14 @@
 import type { BaseQueryFn } from "@reduxjs/toolkit/query";
-import axiosInstance from "../api/axiosInstance";
+import axiosInstance from "./axiosInstance";
 import type { AxiosRequestConfig, AxiosError } from "axios";
 
-export const axiosBaseQuery = (): BaseQueryFn<
-  {
+export const axiosBaseQuery =
+  (): BaseQueryFn<{
     url: string;
     method: AxiosRequestConfig["method"];
     body?: AxiosRequestConfig["data"];
     params?: AxiosRequestConfig["params"];
-  },
-  unknown,
-  unknown
-> =>
+  }> =>
   async ({ url, method, body, params }) => {
     try {
       const result = await axiosInstance({ url, method, data: body, params });
